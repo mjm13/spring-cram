4da6719ffade7c7671eace001f623b5aba0025a0
MeiJM <mjm13@126.com>
2020-11-04 17:57:47 +0800

    初始化

---
+++ statemachine/src/main/java/com/meijm/statemachine/StatemachineApplication.java
@@ -0,0 +1,13 @@
+package com.meijm.statemachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatemachineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StatemachineApplication.class, args);
+    }
+
+}
