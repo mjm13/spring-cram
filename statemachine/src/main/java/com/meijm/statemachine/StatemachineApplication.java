efbd7c9ae84369bb70bfbf0fa40b1c16fe235803
MeiJM <mjm13@126.com>
2020-11-10 17:20:33 +0800

    更新代码

--- statemachine/src/main/java/com/meijm/statemachine/StatemachineApplication.java
+++ statemachine/src/main/java/com/meijm/statemachine/StatemachineApplication.java
@@ -1,11 +1,17 @@
 package com.meijm.statemachine;
 
+import com.meijm.statemachine.config.Events;
+import com.meijm.statemachine.config.States;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.config.StateMachineFactory;
+import org.springframework.statemachine.persist.StateMachinePersister;
 
 @SpringBootApplication
 public class StatemachineApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(StatemachineApplication.class, args);
     }
---
+++ statemachine/src/main/java/com/meijm/statemachine/config/Events.java
@@ -0,0 +1,5 @@
+package com.meijm.statemachine.config;
+
+public enum Events {
+    E1, E2
+}
