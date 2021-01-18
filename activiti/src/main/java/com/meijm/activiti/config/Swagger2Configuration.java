dad2dc13b978fb60dc337c16dfd8fbf57e22a500
MeiJM <mjm13@126.com>
2021-01-18 17:49:01 +0800

    activiti驳回任务

---
+++ activiti/src/main/java/com/meijm/activiti/config/Swagger2Configuration.java
@@ -0,0 +1,53 @@
+/**
+ * This file created at 2017年6月23日.
+ *
+ * Copyright (c) 2002-2017 crisis, Inc. All rights reserved.
+ */
+package com.meijm.activiti.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.Parameter;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <code>{Swagger2Configuration}</code>
+ *
+ * @author chenke
+ */
+@Configuration
+@EnableSwagger2
+public class Swagger2Configuration {
+
+	@Bean
+    public Docket createRestApi() {
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.meijm.activiti.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("流程引擎")
+                .contact(new Contact("", null, null))
+                .version("1.0")
+                .build();
+    }
+
+}
--- activiti/src/main/java/com/meijm/activiti/controller/BaseActivitiController.java
+++ activiti/src/main/java/com/meijm/activiti/controller/BaseActivitiController.java
@@ -2,9 +2,12 @@ package com.meijm.activiti.controller;
 
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.collection.CollectionUtil;
+import com.meijm.activiti.common.flow.RejectTask;
 import org.activiti.engine.ActivitiObjectNotFoundException;
+import org.activiti.engine.ManagementService;
 import org.activiti.engine.RuntimeService;
 import org.activiti.engine.TaskService;
+import org.activiti.engine.impl.identity.Authentication;
 import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.task.Task;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,11 +29,15 @@ public class BaseActivitiController {
     @Autowired
     private TaskService taskService;
 
+    @Autowired
+    private ManagementService managementService;
+
     //发起一个流程
     @RequestMapping(value = "start", method = RequestMethod.GET)
-    public Map<String, Object> start() {
+    public Map<String, Object> start(String businessKey) {
         //mjm businessKey为任意值,表示与业务关联的信息,便于集成
-        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("SimpleProcess_id", "mjm");
+        Authentication.setAuthenticatedUserId("admin");
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("SimpleProcess_id", businessKey);
         return BeanUtil.copyProperties(processInstance, Map.class, "currentFlowElement", "subProcessInstance", "identityLinks", "executions");
     }
 
@@ -38,7 +45,7 @@ public class BaseActivitiController {
     @RequestMapping(value = "taskList", method = RequestMethod.GET)
     public List<Map<String, Object>> taskList() {
         //mjm 为流程定义中分配的人员 对应 processes/SimpleProcess.bpmn:5
-        List<Task> tasks = taskService.createTaskQuery().taskAssignee("admin").list();
+        List<Task> tasks = taskService.createTaskQuery().list();
         return tasks.stream().map(task -> {
             Map<String, Object> data = new HashMap<>();
             data = BeanUtil.copyProperties(task, Map.class, "execution", "processInstance", "variableInstances");
@@ -46,15 +53,27 @@ public class BaseActivitiController {
         }).collect(Collectors.toList());
     }
 
-    //查看指定用户任务列表
+    //完成任务
     @RequestMapping(value = "completeTask", method = RequestMethod.GET)
     public String completeTask(String processId) {
-        List<Task> taskList = taskService.createTaskQuery().processInstanceId(processId).taskAssignee("admin").list();
+        Authentication.setAuthenticatedUserId("王五");
+        List<Task> taskList = taskService.createTaskQuery().processInstanceId(processId).list();
         if (CollectionUtil.isEmpty(taskList)) {
             throw new ActivitiObjectNotFoundException("processId:" + processId + "-Assignee:admin");
         }
         taskService.complete(taskList.get(0).getId());
         //mjm 为流程定义中分配的人员 对应 processes/SimpleProcess.bpmn:5
+        return "操作完成!";
+    }
+
+    @RequestMapping(value = "rejectTask", method = RequestMethod.GET)
+    public String rejectTask(String processId) {
+        Authentication.setAuthenticatedUserId("王五");
+        List<Task> taskList = taskService.createTaskQuery().processInstanceId(processId).list();
+        if (CollectionUtil.isEmpty(taskList)) {
+            throw new ActivitiObjectNotFoundException("processId:" + processId + "-Assignee:admin");
+        }
+        managementService.executeCommand(new RejectTask(taskList.get(0).getId(),new HashMap<>(),"驳回"));
         return "操作完成!";
     }
 
