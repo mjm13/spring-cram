84a9f89fe2a90d8109fdb732e0a14501c3dfd7b0
meijianming <mjm13@126.com>
2023-08-02 17:53:56 +0800

    增加stomp项目

--- basis-java/src/main/java/com/meijm/basis/thread/ThreadTest.java
+++ basis-java/src/main/java/com/meijm/basis/thread/ThreadTest.java
@@ -1,4 +1,26 @@
 package com.meijm.basis.thread;
 
 public class ThreadTest {
+
+    public static void main(String[] args) throws InterruptedException {
+        TestThread testThread = new TestThread();
+        testThread.start();
+        Thread.sleep(100L);
+        System.out.println(testThread.isAlive());
+        testThread.myactive = false;
+        Thread.sleep(1000L);
+        System.out.println(testThread.isAlive());
+    }
+}
+
+class TestThread extends Thread{
+    public volatile boolean myactive = true;
+
+    @Override
+    public void run() {
+        while (myactive) {
+            System.out.println(111);
+        }
+
+    }
 }
