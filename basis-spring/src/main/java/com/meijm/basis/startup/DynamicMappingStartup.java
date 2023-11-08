75bef0151e5ed17d2f3de85b4162c47e08b160c2
meijianming <mjm13@126.com>
2023-11-08 09:26:40 +0800

    使用启动项初始化动态链接

--- basis-spring/src/main/java/com/meijm/basis/startup/DynamicMappingStartup.java
+++ basis-spring/src/main/java/com/meijm/basis/startup/DynamicMappingStartup.java
@@ -1,35 +1,33 @@
-package com.meijm.basis.listener.spring;
+package com.meijm.basis.startup;
 
 import com.meijm.basis.annotation.DynamicMapping;
 import com.meijm.basis.annotation.DynamicMappingUrl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.ApplicationContext;
 import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 import org.springframework.web.util.pattern.PathPatternParser;
 
 import java.util.Map;
 
-/**
- * 测试动态注册链接
- */
-//@Slf4j
-//@Component
-public class DynamicMappingListener implements ApplicationListener<ContextRefreshedEvent> {
+@Slf4j
+@Component
+public class DynamicMappingStartup implements ApplicationRunner {
 
-//    @Autowired
+    @Autowired
+    private ApplicationContext applicationContext;
+    
+    @Autowired
     private RequestMappingHandlerMapping handlerMapping;
-
+    
     @Override
-    public void onApplicationEvent(ContextRefreshedEvent event) {
-//        log.info("注册日志：{}", event.getSource().getClass().toString());
-        
+    public void run(ApplicationArguments args) throws Exception {
         handlerMapping.getHandlerMethods().
                 entrySet().stream().
                 filter(entry -> AnnotationUtils.getAnnotation(entry.getValue().getMethod(), DynamicMapping.class) != null).
@@ -39,7 +37,7 @@ public class DynamicMappingListener implements ApplicationListener<ContextRefres
                         options.setPatternParser(new PathPatternParser());
                         String path = entry.getKey().getDirectPaths().stream().findFirst().orElse(null);
                         String beanName = (String) entry.getValue().getBean();
-                        DynamicMappingUrl dmu = new DynamicMappingUrl(entry.getValue().getMethod(),event.getApplicationContext().getBean(beanName));
+                        DynamicMappingUrl dmu = new DynamicMappingUrl(entry.getValue().getMethod(),applicationContext.getBean(beanName));
                         RequestMappingInfo info = RequestMappingInfo
                                 .paths(path+"_mjm")
                                 .methods(RequestMethod.GET)
@@ -58,5 +56,4 @@ public class DynamicMappingListener implements ApplicationListener<ContextRefres
                     }
                 });
     }
-
 }
