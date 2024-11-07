4eb2e5d2df9abcb1651c8ea564a5794574d2640e
meijianming <mjm13@126.com>
2024-11-07 11:12:52 +0800

    测试异常定时任务

--- basis-spring/src/main/java/com/meijm/basis/task/CronTask.java
+++ basis-spring/src/main/java/com/meijm/basis/task/CronTask.java
@@ -4,22 +4,32 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.sql.SQLException;
+
 /**
  * 测试执行是否会重复触发
  */
 @Slf4j
 @Component
 public class CronTask {
-//    @Scheduled(cron="*/1 * * * * *")
+    @Scheduled(cron="*/1 * * * * *")
     public void doSomething() throws InterruptedException {
-        log.info("任务执行中");
+        log.info("任务执行中111111111111111");
         Thread.sleep(2000);
     }
 
 
-//    @Scheduled(fixedRate = 5000)
-    public void test() {
-        log.info("任务执行中");
-        throw new RuntimeException("111");
+    @Scheduled(fixedRate = 1000)
+    public void test() throws Exception {
+        log.info("任务执行中222222222222");
+        Thread t = new Thread(() -> {
+            try {
+                Thread.sleep(3000);
+                throw new SQLException("111");
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+        t.start();
     }
 }
--- integration-middleware/rabbitmq/src/main/java/com/meijm/rabbitmq/controller/DemoController.java
+++ integration-middleware/rabbitmq/src/main/java/com/meijm/rabbitmq/controller/DemoController.java
@@ -1,6 +1,7 @@
 package com.meijm.rabbitmq.controller;
 
 import cn.hutool.json.JSONUtil;
+import com.meijm.rabbitmq.config.DynamicContainer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageBuilder;
@@ -9,6 +10,7 @@ import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -24,10 +26,12 @@ public class DemoController {
 
     @Autowired
     private RabbitTemplate rabbitTemplate;
+    @Autowired
+    private ApplicationContext applicationContext;
 
     @RequestMapping(value = "demo", method = RequestMethod.GET)
-    public String start() {
-        for (int i = 0; i < 10; i++) {
+    public String start() throws InterruptedException {
+        for (int i = 0; i < 100; i++) {
             MessageProperties messageProperties = new MessageProperties();
             messageProperties.setPriority(i); // 设置消息优先级
             Map<String,String> data = new HashMap<>();
@@ -35,14 +39,19 @@ public class DemoController {
             MessageConverter messageConverter = rabbitTemplate.getMessageConverter();
             rabbitTemplate.send("plg-yc-exchage","plg-yc-m-test" + 0,messageConverter.toMessage(data,messageProperties) );  
         }
-
-//        for (int i = 0; i <1 ; i++) {
-//            Map<String,String> data = new HashMap<>();
-//            data.put("msg","测试消息"+i);
-//            //同步消息发送
-//            rabbitTemplate.convertAndSend("plg-yc-exchage","plg-yc-m-test" + 0, data);
-//        }
+        Thread.sleep(1000L);
         return "发送完成!";
+    }
+    @RequestMapping(value = "dynamic", method = RequestMethod.GET)
+    public String dynamic(int count) {
+        DynamicContainer dynamicContainer = (DynamicContainer) applicationContext.getBean("mjmm");
+        dynamicContainer.setConcurrentConsumers(count);
+//        dynamicContainer.stop(() -> {
+//            dynamicContainer.setConcurrentConsumers(5);
+//            dynamicContainer.setConcurrency("5-10");
+//            dynamicContainer.start();
+//        });
+        return "动态接收完成!";
     }
 
     @RequestMapping(value = "deadletter", method = RequestMethod.GET)
