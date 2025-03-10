7e4e732e816fb26e031acb910a225a1342e69cd1
meijianming <mjm13@126.com>
2025-03-10 14:36:05 +0800

    demo

--- basis-java/src/main/java/com/meijm/basis/Hello.java
+++ basis-java/src/main/java/com/meijm/basis/Hello.java
@@ -2,6 +2,15 @@ package com.meijm.basis;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello Java");
+        System.out.println("1111111111111111111");
+        test:{
+            for (int i = 0; i < 10; i++) {
+                System.out.println(i);
+                if(i == 5){
+                    break test;
+                }
+            }
+        }
+        System.out.println("222222222222222222222");
     }
 }
